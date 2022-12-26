2d3878ae12ea7f4aeed04dae739769ead41db496
Daniel Ibeh <dibeh1000@gmail.com>
2022-12-26 07:33:33 +0100

    added error message

--- src/routes/api/errors.ts
+++ src/routes/api/errors.ts
@@ -9,12 +9,20 @@ const errors = (
   const height: number = parseInt(req.query.height as unknown as string);
   const width: number = parseInt(req.query.width as unknown as string);
   const filename = req.query.filename as unknown as string;
-  if (isNaN(width)) {
-    res.status(400).send({ error: "Invalid input for width" });
+  if (!filename && !width && !height) {
+    res.status(400).send({ error: "Missing filename, height and width" });
     return;
   }
-  if (isNaN(height)) {
-    res.status(400).send({ error: "Invalid input for height" });
+  if (!filename) {
+    res.status(400).send({ error: "Invalid input for filename " });
+    return;
+  }
+  if (!width) {
+    res.status(500).send("Invalid input for width");
+    return;
+  }
+  if (!height) {
+    res.status(500).send("Invalid input for height");
     return;
   }
   next();
